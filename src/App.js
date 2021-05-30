e4d713a33ab47242a0ba1724d9e3390180cce9b0
vansinh <tvsinh.007@gmail.com>
2021-05-30 16:42:41 +0700

    color box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.scss';
 import React from 'react';
+import ColorBox from './components/ColorBox';
 
 
 function App() {
   return (
     <div className="app">
       <h2>Wellcome</h2>
+
+      <ColorBox />
     </div>
   );
 }
---
+++ src/components/ColorBox/index.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './ColorBox.scss';
+
+ColorBox.propTypes = {
+
+};
+
+function getRandomColor() {
+    const COLOR_LIST = ['green', 'black', 'deeppink', 'yellow', 'blue'];
+    const randomIndex = Math.trunc(Math.random() * 5);
+    return COLOR_LIST[randomIndex];
+}
+
+function ColorBox() {
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem('box_color') || 'deeppink';
+        return initColor;
+    });
+
+    function handleBoxClick() {
+        const newColor = getRandomColor();
+        setColor(newColor);
+        localStorage.setItem('box_color', newColor);
+    }
+    return (
+        <div
+            className="color-box"
+            style={{ backgroundColor: color }}
+            onClick={handleBoxClick}
+        >
+            <p>COLOR BOX</p>
+        </div>
+    );
+}
+
+export default ColorBox;
